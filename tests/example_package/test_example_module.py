b1fa80f7994a71f03583c72f66222c7c17f4930b
Tim Lister <tlister@lco.global>
2024-01-09 15:56:53 -0800

    Convert PosixPath to str in call to read_element. Fix missing imports

--- tests/example_package/test_example_module.py
+++ tests/example_package/test_example_module.py
@@ -1,5 +1,6 @@
 import os
 
+from astropy.tests.helper import assert_quantity_allclose
 from importlib_resources import files
 from synphot import units
 from synphot.spectrum import BaseUnitlessSpectrum, SourceSpectrum, SpectralElement
@@ -27,9 +28,9 @@ class TestReadElement:
         test_fp = files("tests.example_package.data").joinpath("test_radiance.dat")
 
         print(f"test_fp= {test_fp}")
-        element = read_element(test_fp, element_type="spectrum")
+        element = read_element(test_fp.as_posix(), element_type="spectrum")
 
         assert isinstance(element, SourceSpectrum)
-        assert element.waveset[0].unit == u.AA
-        assert_quantity_allclose(element.waveset[0], 300 * u.nm)
+        assert element.waveset[0].unit == units.u.AA
+        assert_quantity_allclose(element.waveset[0], 300 * units.u.nm)
         assert_quantity_allclose(element(element.waveset[0]), 16.4805 * units.PHOTLAM, rtol=1e-3)
